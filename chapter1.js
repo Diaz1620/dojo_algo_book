680f6e7ed8f82498a7ad73fd28262b917f7c1aa7
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2022-10-25 19:09:54 -0400

    added more

--- chapter1.js
+++ chapter1.js
@@ -113,39 +113,39 @@
 // Statistics to Doubles
 // Implement a ‘die’ that randomly returns an integer between 1 and 6 inclusive. Roll a pair of these dice, tracking the statistics until doubles are rolled. Display the ​number of rolls​, ​min,​ ​max,​ and ​average​.
 
-var numRolls = 0
-var min = 0, max = 0, avg = 0
-var numRollsTillDouble = []
+// var numRolls = 0
+// var min = 0, max = 0, avg = 0
+// var numRollsTillDouble = []
 
-function rollDie(){
-    // console.log(Math.floor(Math.random() * 6) + 1);
-    return Math.floor(Math.random() * 6) + 1
-}
+// function rollDie(){
+//     // console.log(Math.floor(Math.random() * 6) + 1);
+//     return Math.floor(Math.random() * 6) + 1
+// }
 
-function rollDice(){
-    numRolls ++
-    let diceResults = [rollDie(), rollDie()]
-    console.log("Current rolls: ", numRolls);
-    console.log(diceResults);
-    if(diceResults[0] == diceResults[1]){
-        console.log("Number of rolls till double: ", numRolls);
-        numRollsTillDouble.push(numRolls)
-        if(min == 0){
-            min = numRolls
-        }
-        if(numRolls > max){
-            max = numRolls
-        }
-        else if(numRolls < min){
-            min = numRolls
-        }
-        // numRolls = 0
-    }
-        console.log("Minimum number of rolls for double so far: ", min);
-        console.log("Maximum number of rolls for double so far: ", max);
-        console.log("Rolls are now: ", numRolls);
-        return numRolls
-}
+// function rollDice(){
+//     numRolls ++
+//     let diceResults = [rollDie(), rollDie()]
+//     console.log("Current rolls: ", numRolls);
+//     console.log(diceResults);
+//     if(diceResults[0] == diceResults[1]){
+//         console.log("Number of rolls till double: ", numRolls);
+//         numRollsTillDouble.push(numRolls)
+//         if(min == 0){
+//             min = numRolls
+//         }
+//         if(numRolls > max){
+//             max = numRolls
+//         }
+//         else if(numRolls < min){
+//             min = numRolls
+//         }
+//         // numRolls = 0
+//     }
+//         console.log("Minimum number of rolls for double so far: ", min);
+//         console.log("Maximum number of rolls for double so far: ", max);
+//         console.log("Rolls are now: ", numRolls);
+//         return numRolls
+// }
 // function diceStatistics(){
 //     let numRolls = count, min = 0, max = 0, avg = 0;
 //     if(rollDie()){
@@ -153,6 +153,46 @@ function rollDice(){
 //     }
 // }
 
-rollDice()
 
+// Sum To One Digit
+// Implement a function ​sumToOne(num)​ that, given a number, sums that number’s digits repeatedly until the sum is only one digit. Return thatfinalonedigitresult.
 
+function sumToOne(num){
+    let stringNum = num.toString().split('');
+    let realNum = stringNum.map(Number)
+    let sum = 0
+    if(num > 9){
+        for(let i = 0; i < realNum.length; i++){
+            sum += realNum[i]
+        }
+        if(sum > 9){
+            sumToOne(sum)
+        }
+        else{
+            console.log("The total sum is now "+sum+ " and it is less than nine");
+            return sum
+        }
+    }
+    else{
+        // console.log("Num is now", num);
+        return num
+    }
+}
+
+console.log(sumToOne(928));
+
+// Internet answer that I don't think matches the description of how the function is supposed to work
+// function sumToOne(n){
+//     let sum = 0
+//     while (n > 0 || sum > 9){
+//         if (n == 0){
+//             n = sum;
+//             sum = 0;
+//         }
+//         sum = sum + n % 10;
+//         n = Math.floor(n/10);
+//     }
+//     return sum;
+// }
+
+// console.log(sumToOne(928));
