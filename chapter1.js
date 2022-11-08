882f97ecbe8d22871b96888b0a6f52e7e61a506a
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2022-11-07 20:11:35 -0500

    added new algo

--- chapter1.js
+++ chapter1.js
@@ -228,3 +228,21 @@ function altLastDigitAtoB(a,b){
 
 console.log(altLastDigitAtoB(12,5));
 
+// PushFront
+// Given an array and an additional value, ​insert this value ​at the beginning of the array. Do this without using any built-in array methods.
+
+function pushFront(arr, val){
+    // arr[arr.length] = val
+    for(let i = arr.length; i > 0; i--){
+        arr[i] = arr[i-1]
+    }
+    arr[0] = val
+    console.log(arr);
+}
+
+pushFront([2,3,4], 1)
+
+
+
+
+
